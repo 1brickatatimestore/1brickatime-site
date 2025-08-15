fadfd86e32da2323101cd386a5963a772c2294ea
Kamila McT <1brickatatimestore@gmail.com>
2025-08-15 16:34:44 +1000

    feat: add SSR plausible tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const domain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN; // "www.1brickatatimestore.com,1brickatatimestore.com"
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {domain && (
+          <script
+            defer
+            data-domain={domain}
+            data-api="https://plausible.io/api/event"
+            src="https://plausible.io/js/script.js"
+          />
+        )}
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
